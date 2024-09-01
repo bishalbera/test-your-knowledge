c0cb59d6fc0981cded0970f7888dbc1593092245
bishalbera <bishalbera2@gmail.com>
2024-09-01 09:58:27 +0530

    feat: modified the profile page and a button for the admin user

--- app/profile/[[...user-id]]/page.tsx
+++ app/profile/[[...user-id]]/page.tsx
@@ -2,7 +2,9 @@ import ProfileBody from "@/components/ProfileBody";
 import { prisma } from "@/utils/db";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 import Link from "next/link";
+import { FaPlus } from "react-icons/fa6";
 
 const addUserToDb = async () => {
   const user = await currentUser();
@@ -26,19 +28,17 @@ const addUserToDb = async () => {
 };
 
 const ProfilePage = async () => {
-  await addUserToDb();
+ await addUserToDb();
 
   const cUser = await currentUser();
   const userData = await prisma.user.findUnique({
     where: {
       clerkId: cUser?.id,
     },
-    select: {
-      Score: true,
-      exams: true,
-      imageUrl: true,
-    },
   });
+
+  const isAdmin = userData?.email == process.env.ADMIN_EMAIL;
+  
   return (
     <div className=" bg-primary-color text-white min-h-screen w-screen">
       <nav>
@@ -60,14 +60,22 @@ const ProfilePage = async () => {
           </li>
         </ul>
       </nav>
-      <div className="mb-8 flex justify-center items-center h-[100px]">
-        <img
+      <div className="my-16 flex justify-center h-[100px]">
+        <Image
           src={userData?.imageUrl ?? ""}
           alt="Profile"
-          className="w-16 h-16 rounded-full object-cover"
+          width={100}
+          height={100}
+          className=" rounded-full object-cover"
         />
       </div>
+      <p className="flex justify-center text-lg font-bold">{userData?.name}</p>
       <ProfileBody userData={userData} />
+      {isAdmin && (
+        <button className="bg-button-color text-white border border-white fixed bottom-4 right-4 p-4 rounded-full shadow-lg hover:bg-button-hover-color focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 active:bg-blue-700">
+          <FaPlus className=" h-6 w-6" />
+        </button>
+      )}
     </div>
   );
 };
--- components/ProfileBody.tsx
+++ components/ProfileBody.tsx
@@ -1,19 +1,19 @@
 const ProfileBody = ({ userData }) => {
   if (!userData) return <p>No user data found.</p>;
   return (
-    <div className="  grid place-items-center">
-      <div className="grid grid-cols-2 gap-4 w-[600px] p-4 border border-red-500 ">
-        <div className="bg-[#1A6566] rounded-lg p-4 grid place-items-center">
+    <div className="  grid justify-center">
+      <div className="grid grid-cols-2 gap-4 w-[600px] p-4 ">
+        <div className="bg-[#13678A] rounded-lg p-4  place-items-center">
           <div className="text-center">
             <div className="text-purple-500 mb-2">📄</div>
-            <h3 className="text-gray-600">Exams</h3>
+            <h3 className="text-gray/70">Exams</h3>
             <p className="text-2xl font-bold">{userData.exams}</p>
           </div>
         </div>
-        <div className="bg-green-400 rounded-lg p-4 grid place-items-center">
+        <div className="bg-[#13678A] rounded-lg p-4  place-items-center">
           <div className="text-center">
             <div className="text-text-color mb=2">💯</div>
-            <h3 className="text-gray-600">Score</h3>
+            <h3 className="text-gray/70">Score</h3>
             <p className="text-2xl font-bold">{userData.Score}</p>
           </div>
         </div>
