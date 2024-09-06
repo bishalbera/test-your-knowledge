ff40845eb9156d47765a8eb557c77c44fee112a8
bishalbera <bishalbera2@gmail.com>
2024-09-06 18:41:12 +0530

    made draft of stripe

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,7 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ImGift } from "react-icons/im";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-06-20",
 });
 
+type RequestData = {
+  examTitle: string;
+  cost: number;
+  imageUrl: string;
+  userId: string;
+};
 
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  const { examTitle, cost, imageUrl, userId }: RequestData = await req.json();
+
+  if (
+    !examTitle ||
+    !cost ||
+    !imageUrl ||
+    !userId 
+  ) {
+    return NextResponse.json({ message: "Please all fields are required" }, { status: 400 })
+    
+  }
+
+  
+  
+};
